05c3e36967fe3bad345b6657a95616139cfb52bc
Abhirup Sadhukhan <abhirup_sadhukhan@mcafee.com>
2020-05-25 12:39:10 +0530

    Poision regression only.

--- MLNetExample/MLNetExample/Program.cs
+++ MLNetExample/MLNetExample/Program.cs
@@ -17,29 +17,13 @@ namespace MLNetExample
             var pipeline = context.Transforms.Concatenate("Features", "YearsOfExperience")
                 .Append(context.Regression.Trainers.LbfgsPoissonRegression());
 
-            var pipelineOnlineGradient = context.Transforms.Concatenate("Features", "YearsOfExperience")
-                .Append(context.Regression.Trainers.OnlineGradientDescent());
-
-            var pipelineSdca = context.Transforms.Concatenate("Features", "YearsOfExperience")
-                .Append(context.Regression.Trainers.OnlineGradientDescent());
-
             var modelPoisonRegression = pipeline.Fit(trainData);
-
-            var modelGradient = pipelineOnlineGradient.Fit(trainData);
-
-            var modelSdca = pipelineSdca.Fit(trainData);
 
             //Evaluate 
             var predictionsPoison = modelPoisonRegression.Transform(trainData);
             var matricesPoison = context.Regression.Evaluate(predictionsPoison);
 
-            var predictionsGradient = modelGradient.Transform(trainData);
-            var matricesGradient = context.Regression.Evaluate(predictionsGradient);
-
-            var predictionsSdca = modelSdca.Transform(trainData);
-            var matricesSdca = context.Regression.Evaluate(predictionsSdca);
-
-            Console.WriteLine($"PR RSquared: {matricesPoison.RSquared}, Gradient RSquared: {matricesGradient.RSquared}, Sdca RSquared: {matricesSdca.RSquared}");
+            Console.WriteLine($"PR RSquared: {matricesPoison.RSquared}");
             //Predict
             var newData = new SalaryData
             {
@@ -48,13 +32,7 @@ namespace MLNetExample
             var predictionFuncPoison = context.Model.CreatePredictionEngine<SalaryData, SalaryPrediction>(modelPoisonRegression);
             var predictionPoison = predictionFuncPoison.Predict(newData);
 
-            var predictionFuncGradient = context.Model.CreatePredictionEngine<SalaryData, SalaryPrediction>(modelGradient);
-            var predictionGradient = predictionFuncPoison.Predict(newData);
-
-            var predictionFuncSdca = context.Model.CreatePredictionEngine<SalaryData, SalaryPrediction>(modelSdca);
-            var predictionSdca = predictionFuncSdca.Predict(newData);
-
-            Console.WriteLine($"Poison Pred: {predictionPoison.PredictedSalary}, Gredient Pred: {predictionGradient.PredictedSalary}, Sdca Prediction: {predictionSdca.PredictedSalary}");
+            Console.WriteLine($"Poison Pred: {predictionPoison.PredictedSalary}");
 
             Console.ReadLine();
         }
